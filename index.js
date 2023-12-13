aab525bb7f504da5a84df9e9e23dd9bd17970ab5
Bryan Reiter <tul13138@temple.edu>
2023-12-13 13:28:16 -0500

    set up command cooldowns

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const token = process.env.TOKEN;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
+client.cooldowns = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
